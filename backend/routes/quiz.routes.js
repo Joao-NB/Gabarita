c85bd0bf5c4fd47b52344d602b2e885f705f6a7f
João-NB <nbjoaoguilherme@gmail.com>
2025-12-12 09:12:46 -0300

    Preparar projeto para deploy na Vercel (serverless)

--- backend/routes/quiz.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { gerarQuiz } from '../controllers/quiz.controller.js';
-
-const router = express.Router();
-
-router.post('/', gerarQuiz);
-
-export default router;
